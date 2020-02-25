a433aefb9dbcd950ff669ed8697258dcafabd3d4
余颖丽 <ylyu@sstir.cn>
2020-02-25 13:50:36 +0800

    :construction:modify task run file desc

--- src/views/task/run/index.vue
+++ src/views/task/run/index.vue
@@ -26,12 +26,11 @@
               1.
               检测流程只支持双端测序的样本，两个文件的前缀请保持相同；勾选时如果遗漏，将无法进行分析！
             </li>
-            <li>2. 测序文件后缀请保证为fastq.gz或fq.gz，其它格式系统可能无法识别</li>
+            <li>2. 测序文件后缀请保证为fq.gz，其它格式系统可能无法识别。</li>
             <li>
-              3.
-              每个样本的检测时间，大约为3小时；全部完成后，系统将通过邮件通知您；每个样本的PDF报告会自动生成在对应测序数据的下面
+              3. 每个样本的检测时间，大约为3小时；全部完成后，系统将通过邮件通知您。
             </li>
-            <li>4. 有任何疑问，可以联系tyzuo@sstir.cn，我们将派出专员为您解答</li>
+            <li>4. 有任何疑问，可以联系tyzuo@sstir.cn，我们将派出专员为您解答。</li>
           </ul>
         </div>
         <div class="btn-wrapper">
@@ -293,10 +292,12 @@ export default {
     .content-wrapper {
       max-width: 1100px;
       width: 100%;
-
+      padding-left: 20px;
+      padding-right: 20px;
       .desc {
         color: rgba(0, 0, 0, 0.5);
         font-size: 16px;
+
         li {
           line-height: 24px;
         }
