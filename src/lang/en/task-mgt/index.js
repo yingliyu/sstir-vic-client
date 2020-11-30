71cc9614afedbeaea02412d191fea0c9581453c3
yingliyu <404997046@qq.com>
2020-11-30 16:08:07 +0800

    modify the restrictions  of runing task and description

--- src/lang/en/task-mgt/index.js
+++ src/lang/en/task-mgt/index.js
@@ -1,4 +1,3 @@
-
 export default {
   lists: {
     toDatas: 'My Files',
@@ -16,10 +15,15 @@ export default {
     selData: 'Choose data',
     selected: 'Chosen data',
     cautions: 'Cautions',
-    desc1: '1. The pipeline only supports pair-end sequencing samples. Please keep the prefix of the two files the same; if you omit any of them, you will not be able to analyze them!',
-    desc2: '2. The suffix of the sequencing file must be in the following four formats (take R1 as an example): _R1.fastq.gz, _R1.fq.gz, _1.fastq.gz or _1.fq.gz. Other formats may not be recognized.',
-    desc3: '3. The whole process generally takes about 3 hours, varied by input file sizes. When task is completed, the system will notify you by email, and a PDF report of each sample will be automatically generated. ',
-    desc4: '4. If you have any questions, please contact tyzuo@sstir.cn, we will send a specialist to answer your questions.',
+    // desc1: '1. The pipeline only supports pair-end sequencing samples. Please keep the prefix of the two files the same; if you omit any of them, you will not be able to analyze them!',
+    // desc2: '2. The suffix of the sequencing file must be in the following four formats (take R1 as an example): _R1.fastq.gz, _R1.fq.gz, _1.fastq.gz or _1.fq.gz. Other formats may not be recognized.',
+    desc1: '',
+    desc2:
+      '1. The suffix of the sequencing file must be in the following formats : .fastq, .fq. Other formats may not be recognized.',
+    desc3:
+      '2. The whole process generally takes about 3 hours, varied by input file sizes. When task is completed, the system will notify you by email, and a PDF report of each sample will be automatically generated. ',
+    desc4:
+      '3. If you have any questions, please contact tyzuo@sstir.cn, we will send a specialist to answer your questions.',
     startRun: 'Start running',
     tips1: 'Please select two gene sequencing data (make sure the prefix name is the same)',
     tips2: 'The task failed to run, please try again!'
